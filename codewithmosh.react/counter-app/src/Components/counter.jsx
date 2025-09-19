0529994915e0ba45188c732f7a5bc503320582f6
Roushan Singh <github.rtron18@gmail.com>
2025-09-19 16:49:49 +0530

    Completed Counter App Project

--- codewithmosh.react/counter-app/src/Components/counter.jsx
+++ codewithmosh.react/counter-app/src/Components/counter.jsx
@@ -2,16 +2,36 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        tags: ["tag1", "tag2", "tag3"]
     };
+
+    incrementCounter = (args) => {
+        console.log(args);
+        this.setState({count: this.state.count + 1});
+    };
+
+    formatCount = () => this.state.count === 0 ? "Zero" : this.state.count;
 
     render() { 
         return (
             <React.Fragment>
-                <span>{count}</span>
-                <button>Increment</button>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm" 
+                    onClick = {() => this.incrementCounter({id: 1})}>
+                        Increment
+                </button>
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>
             </React.Fragment>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 ";
+        classes += (this.state.count === 0 ? "badge-warning" : "badge-primary");
+        return classes;
     }
 }
  
--- codewithmosh.react/counter-app/src/index.js
+++ codewithmosh.react/counter-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './Components/counter';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

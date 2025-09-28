fa577c6155af1a24b87d81a41a4705d43fc38b63
Roushan Singh <github.rtron18@gmail.com>
2025-09-28 14:32:46 +0530

    Add Object Destructuring in Counters Component

--- codewithmosh.react/counter-app/src/Components/counters.jsx
+++ codewithmosh.react/counter-app/src/Components/counters.jsx
@@ -3,19 +3,20 @@ import Counter from './counter';
 
 class Counters extends Component {
     render() { 
+        const {onDelete, onReset, onIncrement, counters} = this.props;
         return (
             <div style = {{ margin: 20}}>
                 <button 
                     className="btn btn-primary btn-sm m-2"
-                    onClick = {this.props.onReset}
+                    onClick = {onReset}
                 >
                     Reset
                 </button>
-                { this.props.counters.map(counter => 
+                { counters.map(counter => 
                     <Counter 
                         key={counter.id} 
-                        onDelete={this.props.onDelete} 
-                        onIncrement = {this.props.onIncrement}
+                        onDelete={onDelete} 
+                        onIncrement = {onIncrement}
                         counter={counter} />
                 )}
             </div>

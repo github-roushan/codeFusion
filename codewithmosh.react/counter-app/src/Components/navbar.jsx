692f3f4de577dd8524facc49947f81b4b586cc4b
Roushan Singh <github.rtron18@gmail.com>
2025-09-28 14:29:29 +0530

    Add Object destructuring

--- codewithmosh.react/counter-app/src/Components/navbar.jsx
+++ codewithmosh.react/counter-app/src/Components/navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const NavBar = (props) => {
-    console.log(props)
+    const {totalCounters} = props;
     return (
         <nav className="navbar navbar-light bg-light">
             <div className="navbar-brand" href="#">
-                Navbar <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
+                Navbar <span className="badge badge-pill badge-secondary">{totalCounters}</span>
             </div>
         </nav>
     );

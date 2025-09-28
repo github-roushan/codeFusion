c0652189ea1804a3505cbab043b4d26befa998cc
Roushan Singh <github.rtron18@gmail.com>
2025-09-28 13:32:16 +0530

    remove args from increment Counter and simplify props

--- codewithmosh.react/counter-app/src/Components/counter.jsx
+++ codewithmosh.react/counter-app/src/Components/counter.jsx
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        value: this.props.value,
+        value: this.props.counter.value,
     };
 
-    incrementCounter = (args) => {
+    incrementCounter = () => {
         this.setState({value: this.state.value + 1});
     };
 
@@ -16,10 +16,10 @@ class Counter extends Component {
             <div>
                 <span style={{ fontSize: 24, width: 100 }} className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-lg" 
-                    onClick = {() => this.incrementCounter({id: 1})}>
+                    onClick = {this.incrementCounter}>
                         Increment
                 </button>
-                <button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
     }

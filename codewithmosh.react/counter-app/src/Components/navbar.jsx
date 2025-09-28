7db2b1ae0aba1fe7b33b6ee546477b15ce11a600
Roushan Singh <github.rtron18@gmail.com>
2025-09-28 14:17:52 +0530

    Update Navbar to stateless functional component

--- codewithmosh.react/counter-app/src/Components/navbar.jsx
+++ codewithmosh.react/counter-app/src/Components/navbar.jsx
@@ -1,16 +1,14 @@
- import React, { Component } from 'react';
+import React, { Component } from 'react';
 
- class NavBar extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">
-                    Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
-                </a>
-            </nav>
-        );
-    }
- }
-  
- export default NavBar;
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+                Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+            </a>
+        </nav>
+    );
+};
+
+
+export default NavBar;

f2b16c23edc44a44af6b3efeb4c8ce8a150ac639
Roushan Singh <github.rtron18@gmail.com>
2025-10-10 19:12:31 +0530

    Improve Components

--- codewithmosh.react/router-app/src/components/products.jsx
+++ codewithmosh.react/router-app/src/components/products.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Products extends Component {
-    state = {  } 
-    render() { 
+    state = {}
+    render() {
         return (
             <div>
-                Products
+                <h1>Products</h1>
+                <ul>
+                    <li><Link to="/products/1">Product 1</Link></li>
+                    <li><Link to="/products/2">Product 2</Link></li>
+                    <li><Link to="/products/3">Product 3</Link></li>
+                </ul>
             </div>
         );
     }
 }
- 
+
 export default Products;

1e2302a68f2f53fe46565d9f25b6d2ee8678c047
Roushan Singh <github.rtron18@gmail.com>
2025-10-10 17:40:59 +0530

    kick: routing app to learn react routing

---
+++ codewithmosh.react/router-app/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Posts</a></li>
+        <li><a href="#">Admin</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;

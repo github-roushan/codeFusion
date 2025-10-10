6b0398f4f99ab1545ae509fc9d799a08561f6d40
Roushan Singh <github.rtron18@gmail.com>
2025-10-10 18:51:34 +0530

    Add some routes

--- codewithmosh.react/router-app/src/App.js
+++ codewithmosh.react/router-app/src/App.js
@@ -1,14 +1,32 @@
-function App() {
-  return (
-    <div className="App">
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Products</a></li>
-        <li><a href="#">Posts</a></li>
-        <li><a href="#">Admin</a></li>
-      </ul>
-    </div>
-  );
+import React, { Component } from 'react';
+import Products from './components/products';
+import Home from './components/home';
+import { Route, Routes, Link } from 'react-router-dom';
+
+class App extends Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/products">Products</Link></li>
+            <li><a href="#">Posts</a></li>
+            <li><a href="#">Admin</a></li>
+          </ul>
+        </nav>
+        
+        <div className="content">
+          <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/home" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+          </Routes>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;

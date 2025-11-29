ec2a8af5a0abdb99380956c3da1aef046c6d7c60
Roushan Singh <github.rtron18@gmail.com>
2025-11-29 20:12:41 +0530

    Refactor leap year

--- Mastering-TypeScript/exercises/func_type_exer.ts
+++ Mastering-TypeScript/exercises/func_type_exer.ts
@@ -6,11 +6,7 @@ console.log(twoFer());
 console.log(twoFer("Roushan"));
 
 function isLeapYear(year: number): boolean {
-    if (year % 4 === 0) {
-        if (year % 400 === 0) return true;
-        if (year % 100 !== 0) return true;
-    }
-    return false;
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 }
 
 console.log(isLeapYear(1998));

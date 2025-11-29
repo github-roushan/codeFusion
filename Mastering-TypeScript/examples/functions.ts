ea6cc0e915de4e7f070d165fa89c0ce40594a48f
Roushan Singh <github.rtron18@gmail.com>
2025-11-29 17:14:09 +0530

    Add Arrow Functions

--- Mastering-TypeScript/examples/functions.ts
+++ Mastering-TypeScript/examples/functions.ts
@@ -7,3 +7,14 @@ function greet(person: string) {
 }
 console.log(greet("Roushan"));
 console.log(square(2));
+
+const add = (a: number, b: number) => {
+    return a + b;
+}
+console.log(add(2, 3));
+
+const doSomething = (person: string, age: number) => {
+    console.log("Doing something");
+}
+
+doSomething("FunnyFace", 20);

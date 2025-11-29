ee8750fed2ceb67196e26661bb2726a58e674600
Roushan Singh <github.rtron18@gmail.com>
2025-11-29 17:08:22 +0530

    Adding Types to function parameters

---
+++ Mastering-TypeScript/examples/functions.ts
@@ -0,0 +1,9 @@
+function square(num: number) {
+    return num * num;
+}
+
+function greet(person: string) {
+    return `Hello ${person}`;
+}
+console.log(greet("Roushan"));
+console.log(square(2));

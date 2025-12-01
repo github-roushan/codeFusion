f7b2559bd64cb752f4fb73f16c03f89d1c892d73
Roushan Singh <github.rtron18@gmail.com>
2025-12-01 17:16:26 +0530

    Add Gotchas

--- Mastering-TypeScript/examples/objects.ts
+++ Mastering-TypeScript/examples/objects.ts
@@ -18,3 +18,15 @@ function randomCoordinate(): { x: number, y: number } {
 }
 
 console.log(randomCoordinate());
+
+// we can pass additonal parameters as part of the object using a declaration but not directly
+//  as long as we pass both first and last
+const singer = { first: "Much", last: "Needed", age: 25, isAlive: true };
+printName(singer);
+
+// The below fails due to missing last
+// const singer = { first: "Much", age: 25, isAlive: true };
+// printName(singer)
+
+// Below Fails as it complains to specify only known parameters
+// printName({ first: "Much", last: "Needed", age: 25, isAlive: true });

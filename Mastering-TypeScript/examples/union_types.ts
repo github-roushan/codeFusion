3a9671256c18ba0afd65f23f146ad36725533b36
Roushan Singh <github.rtron18@gmail.com>
2025-12-01 18:54:44 +0530

    Narrow types before Operations

--- Mastering-TypeScript/examples/union_types.ts
+++ Mastering-TypeScript/examples/union_types.ts
@@ -1,8 +1,25 @@
 import { Point, Loc } from "./all_types";
-let age: number | string = 21;
+type ageType = number | string;
+let age: ageType = 21;
 age = "Twenty One";
 
 console.log(age);
 
 let coordinates: Point | Loc = { x: 1, y: 34 };
 coordinates = { lat: 32.124, long: -32.124 };
+
+function printAge(age: ageType): void {
+    console.log(`Age is ${age}`);
+}
+
+// type narrowing using typeof
+function calcTax(price: number | string) {
+    if (typeof price === "string") {
+        price.replace("$", "");
+        price = parseInt(price);
+    }
+    else if (typeof price === "number") {
+        return price * 0.08;
+    }
+    return price;
+}

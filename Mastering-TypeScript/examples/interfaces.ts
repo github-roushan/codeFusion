e0469f0c7c6942a08b43bc615831c91d0e5599ec
Roushan Singh <github.rtron18@gmail.com>
2025-12-02 00:31:26 +0530

    Add Optional and Readonly Interface Properties

--- Mastering-TypeScript/examples/interfaces.ts
+++ Mastering-TypeScript/examples/interfaces.ts
@@ -1,16 +1,28 @@
 
 interface Person {
-    name: string;
-    age: number;
+    readonly id: number;
+    first: string;
+    age?: number;
+    last: string;
+    nickname?: string;
 }
 
 const wishBirthday = (person: Person) => {
-    console.log(`Happy Birthday ${person.name}! You are ${person.age} years old.`);
+    console.log(`Happy Birthday ${person.first}! You are ${person.age} years old.`);
 }
 
 const person: Person = {
-    name: "John",
+    id: 1,
+    first: "John",
+    last: "Doe",
     age: 21
-}
+};
+
+const person2: Person = {
+    id: 2,
+    first: "Thomas",
+    last: "Hardy",
+    nickname: "Tom"
+};
 
 wishBirthday(person);

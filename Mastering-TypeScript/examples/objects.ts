df6cfd8b1144c4dc9a45e86e22b1ee78b1ba6906
Roushan Singh <github.rtron18@gmail.com>
2025-12-01 17:48:36 +0530

    Intersection Types

--- Mastering-TypeScript/examples/objects.ts
+++ Mastering-TypeScript/examples/objects.ts
@@ -64,3 +64,42 @@ const user: User = {
 console.log(user);
 // below is illegal
 // user.id = 12;
+
+
+// Example of Intersection Types
+type Circle = {
+    radius: number;
+}
+
+type Colorful = {
+    color: string;
+}
+
+// Intersection Type (Bring properties from both the types)
+// Show error if there is one key with different type in each of the Types
+// if key has same type no problem. Key is brought here one time
+type ColorfulCircle = Circle & Colorful;
+
+const happyFace: ColorfulCircle = {
+    radius: 4,
+    color: "yellow",
+};
+
+console.log(happyFace);
+
+type Cat = {
+    numLives: number;
+};
+type Dog = {
+    breed: string;
+};
+// add additional properties
+type CatDog = Cat & Dog & { age: number };
+
+const newBreed = {
+    numLives: 7,
+    breed: "Husky",
+    age: 9
+};
+
+console.log(newBreed);

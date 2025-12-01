0579ba9aef48ba9f6024c28a2934d6866d0d1ab1
Roushan Singh <github.rtron18@gmail.com>
2025-12-01 22:19:23 +0530

    Add HttpStatus Array

--- Mastering-TypeScript/examples/tuples.ts
+++ Mastering-TypeScript/examples/tuples.ts
@@ -7,3 +7,8 @@ let resp_status: HTTPStatus = [200, "OK"];
 resp_status = [404, "Not Found"];
 
 console.log(resp_status)
+const responses: HTTPStatus[] = [
+    [200, "OK"],
+    [404, "Not Found"],
+    [500, "Internal Server Error"],
+];

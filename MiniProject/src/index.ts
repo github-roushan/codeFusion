759023dedbe6b5b056aba0b68edfd5f6a55718b4
Roushan Singh <github.rtron18@gmail.com>
2025-12-10 15:16:55 +0530

    Add Non Null Assertion Operator

--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -7,6 +7,13 @@ printDouble("Hello World");
 const btn = document.getElementById("btn");
 console.log(btn);
 
+// ? is called optional chaining operator
 btn?.addEventListener("click", () => {
     console.log("Button Clicked");
+});
+
+// we can also use the non null assertion operator
+const btn_sure = document.getElementById("btn")!;
+btn_sure.addEventListener("click", () => {
+    console.log("Button Sure Clicked");
 });
